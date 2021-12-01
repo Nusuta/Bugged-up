5e9448021080485c53363e93fc8f3dccbc481ecb
atharva bhandvalkar <atharv.bhandvalkar@gmail.com>
2021-12-01 23:59:11 +0530

    init

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    [Range(1,10)]
+    public float smoothFactor;
+    
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+    
+    void Follow()
+    {
+        
+        Vector3 targetPosition = target.position + offset;
+        
+        
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition,smoothFactor*Time.fixedDeltaTime);
+        transform.position = smoothPosition;
+    }
+}
+
+
+/*
+// For camera bound
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    [Range(1,10)]
+    public float smoothFactor;
+    public Vector3 minValues, maxValues;
+    
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+    
+    void Follow()
+    {
+        
+        Vector3 targetPosition = target.position + offset;
+        
+        Vector3 boundPosition = new Vector3(Mathf.Clamp(targetPosition.x,minValues.x,maxValues.x),
+        Mathf.Clamp(targetPosition.y,minValues.y,maxValues.y),
+        Mathf.Clamp(targetPosition.z,minValues.z,maxValues.z));
+        
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition,smoothFactor*Time.fixedDeltaTime);
+        transform.position = smoothPosition;
+    }
+}
+*/
---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    [SerializeField] private float leftside;
+    [SerializeField] private float rightside;
+    [SerializeField] private float movement;
+    [SerializeField] private LayerMask ground;
+
+
+    private Collider2D coll;
+    private Rigidbody2D rb;
+    private bool facingleft = true;
+
+    private void Start()
+    {
+        coll = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+       move();
+    }
+
+    private void move()
+    {
+         if(facingleft)
+        {
+
+            if(transform.position.x > leftside)
+            {
+                if(transform.localScale.x != 1)
+                {
+                    transform.localScale = new Vector3(1, 1);
+                }
+
+                if(coll.IsTouchingLayers(ground))
+                {
+                    rb.velocity = new Vector2(-movement, 0);
+                }
+            }
+            else{
+                facingleft = false;
+            }
+        }
+        else
+        {
+            if(transform.position.x < rightside)
+            {
+                if(transform.localScale.x != -1)
+                {
+                    transform.localScale = new Vector3(-1, 1);
+                }
+
+                 if(coll.IsTouchingLayers(ground))
+                {
+                    rb.velocity = new Vector2(movement, 0);
+                }
+            }
+            else{
+                facingleft = true;
+            }
+        }
+    }
+}
